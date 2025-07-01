a61a87fb1bf2e67acdbb964653ce6dca9344fbfd
leandrapalmeiras <leandra.pereira.cruz@escola.pr.gov.br>
2025-07-01 08:42:11 -0300

    dom

---
+++ main.js
@@ -0,0 +1,11 @@
+const botoest1= document.querySelectorAll(".bottao");
+
+for(let = 0; i< botoest1.length; i++){
+    botoes(1).onclick = function(){
+        for(let ) = 0; 1 < botoes: length; i++) {
+            botoes [i]. onclick + function(){
+                for (let j= 0; 1 < botoes.length; j++)
+            }
+        }
+    }
+}
