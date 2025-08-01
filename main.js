5c70b88df757e7970ba85e13c95044ee8cc30c71
leandrapalmeiras <leandra.pereira.cruz@escola.pr.gov.br>
2025-08-01 07:46:15 -0300

    setInterval

--- main.js
+++ main.js
@@ -12,8 +12,15 @@ for(let i = 0; i< botoes.length; i++){
     }
 }
 
+
+
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-10-05t00:00:00");
+const tempoObjetivo2 = new Date("2025-12-05t00:00:00");
+const tempoObjetivo3 = new Date("2025-12-20t00:00:00");
+const tempoObjetivo4 = new Date("2026-20-05t00:00:00");
+
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4]
 
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 
@@ -29,6 +36,20 @@ function calculaTempo(tempoObjetivo){
     minutos = 60;
     horas = 24;
 
+    if (tempoFinal > 0)
     return dias + " dias " + horas + " horas " + minutos + " minutos " + " segundos ";
-
+ }else{
+         return "Prazo Finalizado";
+    }
 }
+function atualizaCronometro(){
+    for (let i = 0; i < contadores.length; i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000);
+}
+comecaCronometro();
